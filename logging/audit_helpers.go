f35f00125714e5581368e6f2a164dccf41d6f02b
Jens Skipr <jens.willemsens@skipr.co>
2020-10-30 15:34:20 +0100

    Add basic AuditMiddleware

--- logging/audit_helpers.go
+++ logging/audit_helpers.go
@@ -5,12 +5,17 @@ package logging
 import (
 	"context"
 	"reflect"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/micro/go-micro/v2/metadata"
 	log "github.com/sirupsen/logrus"
 )
+
+// ========================================
+// =               CONSTANTS              =
+// ========================================
 
 type auditCategory string
 
@@ -22,25 +27,55 @@ const (
 	auditCategoryFail    auditCategory = "fail"
 )
 
+// ========================================
+// =         START / END OPERATION        =
+// ========================================
+
+// AuditOperationAttempt logs an audit message in the "attempt" category for an operation.
+// This call should be used at the start of an operation.
+func AuditOperationAttempt(ctx context.Context, additionalData map[string]interface{}) {
+	AuditAttempt(ctx, "operation_attempt", additionalData)
+}
+
+// AuditOperationSuccess logs a successful result for a previous call to AuditOperationAttempt.
+// This call should be used at the end of an operation.
+func AuditOperationSuccess(ctx context.Context, additionalData map[string]interface{}) {
+	AuditSuccess(ctx, "operation_success", additionalData)
+}
+
+// AuditOperationFail logs a failure result for a previous call to AuditOperationAttempt
+// This call should be used at the end of an operation.
+func AuditOperationFail(ctx context.Context, additionalData map[string]interface{}) {
+	AuditFail(ctx, "operation_fail", additionalData)
+}
+
+// ========================================
+// =           DURING OPERATION           =
+// ========================================
+
 // AuditFact logs an audit message in the "fact" category
 func AuditFact(ctx context.Context, message string, additionalData map[string]interface{}) {
 	logEvent(ctx, message, auditCategoryFact, additionalData)
 }
 
 // AuditAttempt logs an audit message in the "attempt" category
-func AuditAttempt(ctx context.Context, message string, additionalData map[string]interface{}) {
-	logEvent(ctx, message, auditCategoryAttempt, additionalData)
+func AuditAttempt(ctx context.Context, attemptName string, additionalData map[string]interface{}) {
+	logEvent(ctx, attemptName, auditCategoryAttempt, additionalData)
 }
 
 // AuditSuccess logs a successful result to a previous audit message of category "attempt"
-func AuditSuccess(ctx context.Context, message string, additionalData map[string]interface{}) {
-	logEvent(ctx, message, auditCategorySuccess, additionalData)
+func AuditSuccess(ctx context.Context, attemptName string, additionalData map[string]interface{}) {
+	logEvent(ctx, attemptName, auditCategorySuccess, additionalData)
 }
 
 // AuditFail logs a failure result to a previous audit message of category "attempt"
-func AuditFail(ctx context.Context, message string, additionalData map[string]interface{}) {
-	logEvent(ctx, message, auditCategoryFail, additionalData)
+func AuditFail(ctx context.Context, attemptName string, additionalData map[string]interface{}) {
+	logEvent(ctx, attemptName, auditCategoryFail, additionalData)
 }
+
+// ========================================
+// =                HELPERS               =
+// ========================================
 
 func logEvent(ctx context.Context, message string, category auditCategory, additionalData map[string]interface{}) {
 	// Log priority
@@ -117,5 +152,6 @@ func deriveOperationTime(fields log.Fields) {
 	}
 
 	// Derive and set operation_time
-	fields["operation_time"] = time.Now().Sub(start).Milliseconds()
+	operationTime := time.Now().Sub(start).Milliseconds()
+	fields["operation_time"] = strconv.FormatInt(operationTime, 10)
 }
---
+++ logging/audit_middleware_test.go
@@ -0,0 +1,113 @@
+package logging
+
+import (
+	"net/http/httptest"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pborman/uuid"
+	log "github.com/sirupsen/logrus"
+	logTest "github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func fixtureRequest() (*httptest.ResponseRecorder, *gin.Context) {
+	rec := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(rec)
+	ctx.Request = httptest.NewRequest("GET", "/", nil)
+	return rec, ctx
+}
+
+func testAssertAndDropDynamicEntryFields(t *testing.T, entry log.Entry) log.Fields {
+	// Duplicate fields
+	fields := make(log.Fields, len(entry.Data))
+	for k, v := range entry.Data {
+		fields[k] = v
+	}
+
+	// Assert operation_id => Valid UUID
+	require.Contains(t, fields, "operation_id")
+	require.NotNil(t, uuid.Parse(fields["operation_id"].(string)), "operation_id should be a valid UUID")
+	delete(fields, "operation_id")
+
+	// Assert operation_start_datetime => Valid RFC3339 and max 2 seconds old
+	require.Contains(t, fields, "operation_start_datetime")
+	start, err := time.Parse(time.RFC3339, fields["operation_start_datetime"].(string))
+	require.Nil(t, err)
+	require.WithinDuration(t, time.Now(), start, 2*time.Second)
+	delete(fields, "operation_start_datetime")
+
+	// Assert operation_time
+	require.Contains(t, fields, "operation_time")
+	operationTime, err := strconv.Atoi(fields["operation_time"].(string))
+	require.Nil(t, err)
+	require.True(t, int64(operationTime) < 2*time.Second.Milliseconds(), "operation_time should be less than 2 seconds")
+	delete(fields, "operation_time")
+
+	// Return remaining fields
+	return fields
+}
+
+func Test_AuditMiddleware_OperationSuccess(t *testing.T) {
+	// Setup test
+	hook := logTest.NewGlobal()
+	_, ctx := fixtureRequest()
+
+	// Call middleware
+	mw := AuditMiddleware("test-operator")
+	mw(ctx)
+
+	// Assert operation attempt
+	require.Len(t, hook.Entries, 2)
+	actualAttempt := testAssertAndDropDynamicEntryFields(t, hook.Entries[0])
+	expected := log.Fields{
+		"operator": "test-operator",
+		"category": auditCategoryAttempt,
+		"message":  "operation_attempt",
+	}
+	assert.Equal(t, expected, actualAttempt)
+
+	// Assert operation success
+	actualResult := testAssertAndDropDynamicEntryFields(t, hook.Entries[1])
+	expected["category"] = auditCategorySuccess
+	expected["message"] = "operation_success"
+	assert.Equal(t, expected, actualResult)
+
+	// Assert combined
+	assert.Equal(t, hook.Entries[0].Data["operation_start_datetime"], hook.Entries[1].Data["operation_start_datetime"])
+	hook.Reset()
+}
+
+func Test_AuditMiddleware_OperationFail(t *testing.T) {
+	// Setup test
+	hook := logTest.NewGlobal()
+	_, ctx := fixtureRequest()
+
+	// Call middleware
+	mw := AuditMiddleware("test-operator")
+	ctx.Status(400)
+	mw(ctx)
+
+	// Assert operation attempt
+	require.Len(t, hook.Entries, 2)
+	actualAttempt := testAssertAndDropDynamicEntryFields(t, hook.Entries[0])
+	expected := log.Fields{
+		"operator": "test-operator",
+		"category": auditCategoryAttempt,
+		"message":  "operation_attempt",
+	}
+	assert.Equal(t, expected, actualAttempt)
+
+	// Assert operation fail
+	actualResult := testAssertAndDropDynamicEntryFields(t, hook.Entries[1])
+	expected["category"] = auditCategoryFail
+	expected["message"] = "operation_fail"
+	assert.Equal(t, expected, actualResult)
+
+	// Assert combined
+	assert.Equal(t, hook.Entries[0].Data["operation_start_datetime"], hook.Entries[1].Data["operation_start_datetime"])
+	hook.Reset()
+}
