6024136c2f3fa323ef38c33997f8df03df3f6330
Jens Skipr <jens.willemsens@skipr.co>
2020-11-02 11:18:29 +0100

    Add metadata helpers to "gin" package

---
+++ gin/fixtures_test.go
@@ -0,0 +1,9 @@
+package gin
+
+func fixtureMetadata() map[string]string {
+	return map[string]string{
+		"test_key_1": "test_key_1_success",
+		"test-key-2": "test-key-2-success",
+		"TestKey3":   "TestKey3Success",
+	}
+}
