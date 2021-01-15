33522949a58e733fe74c39271ee50b914936b5da
xavier <xroba@rubbik.com>
2021-01-15 14:16:52 +0100

    add clean Phone number function

---
+++ converters/phone_number_test.go
@@ -0,0 +1,22 @@
+package converters
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_CleanPhoneNumber_success(t *testing.T){
+	result := CleanPhoneNumber("+32475000001")
+	assert.Equal(t, "+32475000001", result)
+}
+
+func Test_CleanPhoneNumberWithInvisibleChar_success(t *testing.T){
+	result := CleanPhoneNumber("+32475000001​")
+	assert.Equal(t, "+32475000001", result)
+}
+
+
+func Test_CleanPhoneNumberWithUnicode_success(t *testing.T){
+	result := CleanPhoneNumber("+32475000001\u200b")
+	assert.Equal(t, "+32475000001", result)
+}
